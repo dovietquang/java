be62b73823f1614fa749a36180dc989be9b3deb9
dovietquang <dovietquang15042004@mail.com>
2024-05-10 10:24:26 +0700

    hi

---
+++ BAITHUCHANH/src/TenClass.java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class TenClass {
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSetInteger = new TreeSet<>();
+        TreeSet<Character> treeSetCharacter = new TreeSet<>(new HashSet<>());
+    }
+}
